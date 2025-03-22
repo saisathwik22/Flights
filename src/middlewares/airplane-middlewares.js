54b3e4eaf80826b0bc2ee6edf13e48169dbe6d64
Sai Sathwik Reddy <reddysaisathwik022@gmail.com>
2025-03-22 07:48:12 +0530

    PHASE 2 - middleware, success, error handling. CodeStage - 23:20

---
+++ src/middlewares/airplane-middlewares.js
@@ -0,0 +1,19 @@
+const { StatusCodes } = require("http-status-codes");
+
+const { ErrorResponse } = require("../utils/common");
+
+function validateCreateRequest(req, res, next) {
+  if (!req.body.modelNumber) {
+    ErrorResponse.message = "Something went wrong while creating aiplane";
+    ErrorResponse.error = {
+      explanation:
+        "Model Number not found in the incoming request in the correct form",
+    };
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  next();
+}
+
+module.exports = {
+  validateCreateRequest,
+};
--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const { AirplaneController } = require("../../controllers");
+const { AirplaneMiddlewares } = require("../../middlewares");
+
 const router = express.Router();
 console.log("inside airplane routes");
+
 // path POST : /api/v1/airplanes
-router.post("/", AirplaneController.createAirplane);
+router.post(
+  "/",
+  AirplaneMiddlewares.validateCreateRequest,
+  AirplaneController.createAirplane
+);
 
 module.exports = router;
